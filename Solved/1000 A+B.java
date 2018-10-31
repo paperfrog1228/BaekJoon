cf7d5e371cb9cbdc0ac90c46decc057ee44e1842
PaperFrog <paperfrog@naver.com>
2018-10-31 18:07:41 +0900

    Delete 1000 A+B.java

--- Solved/1000 A+B.java
+++
@@ -1,8 +0,0 @@
-import java.util.*;
-import java.io.*;
-class Main
-{
-    static Scanner scan = new Scanner(System.in);
-    public static void main(String[] args) {
-        System.out.println(scan.nextInt()+scan.nextInt());
-    }}
